8a32105f108db5ae74a5e0e724d32c43ca157526
sanionwill <734407341@qq.com>
2022-03-20 21:06:37 +0800

    add demo md5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+#region Md5 Demo
+{
+    Md5Helper md5Helper = new Md5Helper();
+    var yourStr = "Hello World";
+    var md5Result = md5Helper.Compute(yourStr);
+    Console.WriteLine($"md5 encrypt result is {md5Result}");
+}
+#endregion
